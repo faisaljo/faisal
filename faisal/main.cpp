bbebedcbae8f17858e66bcca2636bf0f17a98c67
faisaljo <juliensfaisal@gmail.com>
2024-10-24 22:50:45 +0700

    Initial Commit

---
+++ faisal/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  faisal
+//
+//  Created by macbook air m2 on 24/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
